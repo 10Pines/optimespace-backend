69f897750eaae81a96f82f52b4cee65f07e49e35
Luca Hazuca <luca.hazuca@10pines.com>
2019-06-13 16:43:46 -0300

    First test passing for conferences/1

--- routes.js
+++ routes.js
@@ -11,6 +11,20 @@ router.get('/login',(req,res)=>{
   res.render('login');
 });
 
+router.get('/conferences/1', (req, res) => {
+  res.json({
+    conferenceId: 1,
+    rooms: [
+      { roomId: 1, name: 'sala roja' }
+    ],
+    schedule: [
+      { startsAt: 9, endsAt: 10, roomId: 1, talk: {name: "Charla copada de agile"} },
+      { startsAt: 9, endsAt: 11, roomId: 2, talk: {name: "Charla ladri de agile"} },
+      { startsAt: 9, endsAt: 12, roomId: 3, talk: {name: "Charla aburrida de agile"} }
+    ]
+  })
+});
+
 
 
 module.exports = router;
--- routes.test.js
+++ routes.test.js
@@ -4,7 +4,7 @@ import routes from './routes';
 
 it('works', () => {
   const app = express();
-  app.use(routes);
+  app.use('/api', routes);
   return supertest(app).get('/api/conferences/1').then(res => {
     expect(res.body).toEqual({
       conferenceId: 1,
@@ -12,9 +12,9 @@ it('works', () => {
         { roomId: 1, name: 'sala roja' }
       ],
       schedule: [
-        { startsAt: 9, endsAt: 10, roomId: 1, talk },
-        { startsAt: 9, endsAt: 11, roomId: 2, talk },
-        { startsAt: 9, endsAt: 12, roomId: 3, talk }
+        { startsAt: 9, endsAt: 10, roomId: 1, talk: {name: "Charla copada de agile"} },
+        { startsAt: 9, endsAt: 11, roomId: 2, talk: {name: "Charla ladri de agile"} },
+        { startsAt: 9, endsAt: 12, roomId: 3, talk: {name: "Charla aburrida de agile"} }
       ]
     });
   });
