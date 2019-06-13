a14edeb79a7165129fa0b46672e70c782128af3c
S Minutoli <sergio.minutoli@gmail.com>
2019-06-13 16:58:57 -0300

    Refactor test to decouple from data

--- routes-api.test.js
+++ routes-api.test.js
@@ -2,20 +2,33 @@ import express from 'express';
 import supertest from 'supertest';
 import routes from './routes-api';
 
-it('works', () => {
-  const app = express();
-  app.use('/api', routes);
-  return supertest(app).get('/api/conferences/1').then(res => {
-    expect(res.body).toEqual({
-      conferenceId: 1,
-      rooms: [
-        { roomId: 1, name: 'sala roja' }
-      ],
-      schedule: [
-        { startsAt: 9, endsAt: 10, roomId: 1, talk: {name: "Charla copada de agile"} },
-        { startsAt: 9, endsAt: 11, roomId: 2, talk: {name: "Charla ladri de agile"} },
-        { startsAt: 9, endsAt: 12, roomId: 3, talk: {name: "Charla aburrida de agile"} }
-      ]
+describe('GET api/conferences', () => {
+  describe('when an id is provided', () => {
+    let idProvided;
+    const subject = () => {
+      const app = express();
+      app.use('/api', routes);
+      return supertest(app).get(`/api/conferences/${idProvided}`);
+    };
+    describe('and the conference exists', () => {
+      beforeEach(() => {
+        idProvided = 1;
+      });
+      it('retreives the correct conference', () => {
+        subject()
+          .then(res => res.body)
+          .then(body => expect(body.conferenceId).toBe(idProvided));
+      });
+      it('has a rooms array', () => {
+        subject()
+          .then(res => res.body)
+          .then(body => expect(Array.isArray(body.rooms)).toBe(true));
+      });
+      it('has a schedule array', () => {
+        subject()
+          .then(res => res.body)
+          .then(body => expect(Array.isArray(body.schedule)).toBe(true));
+      });
     });
   });
 });
